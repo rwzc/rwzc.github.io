dca0607f986a4d074d5cb63f449c9b9210aa3d41
Ravenoxidal <xiaoblade4@hotmail.com>
2017-01-25 15:04:27 -0800

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -37,5 +37,6 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
+var deadline = 'March 3 2017 23:59:59 GMT-0800';
 var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
 initializeClock('clockdiv', deadline);
